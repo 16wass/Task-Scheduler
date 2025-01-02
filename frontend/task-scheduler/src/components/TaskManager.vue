79ced444f2147d6a30d8c430c04e5e39f94b6206
mezghanni <wassim.wmez@gmail.com>
2025-01-02 18:03:44 +0100

    Frontend task manager component

--- frontend/task-scheduler/src/components/TaskManager.vue
+++ frontend/task-scheduler/src/components/TaskManager.vue
@@ -1,11 +1,30 @@
-<script setup lang="ts">
-
+<script >
+export default {
+  data(){
+    return{
+      tasks:[],
+      new:{
+        title: "",
+        description :""
+      }
+    };
+  }
+}
 </script>
 
 <template>
-
+  <div>
+    <h1> Task manager </h1>
+    <ul>
+      <li v-for=" task in tasks" : key="task.id">{{task.title}}} </li>
+    </ul>
+    <form>
+      <input v-model="new.title" placeholder="Task Title"/>
+      <input v-model="new.description" placeholder="Description" />
+      <button type = "submit" > Add Task </button>
+    </form>
+  </div>
 </template>
 
 <style scoped>
-
 </style>
